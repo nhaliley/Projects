c554015cca1e6ccfad7e89251e1db3597e944d91
Nazariy Haliley <nazariyhaliley@gmail.com>
2019-07-19 21:33:19 -0700

    Yeet.
    Added a way to generate a password while copying it to a file and clipboard.
    Added a command to encrypt and decrypt the file with passwords

--- Password Generator/passGen.sh
+++ Password Generator/passGen.sh
@@ -2,14 +2,33 @@
 
 #Random password generator that copies to a clipboard
 
-printf "Select a length between 10-32. \n"
-read pwLength
+'
+TODO:
 
-generator(){
-  local result=`openssl rand -base64 $pwLength`
-  echo $result | pbcopy
-  echo $result
-}
+[X] Varibale to name text file
+[X] Function to encrypt text file
+[X] Function to decrypt text file
+[X] Function to append into the txt file
+[] PGP way of encrypting the file maybe?
+[] yeet
 
-printf "Password: $(generator) \n"
-printf "Copied to the clipboard!"
+
+'
+FILE=secure.txt
+printf "Select an option: [gen/view] \n"
+read answer
+
+if [[ $answer == "gen" ]]; then
+  printf "Select a length between 10-32. \n"
+  read pwLength
+  openssl rand -base64 $pwLength | pbcopy
+  pbpaste >> $FILE
+  printf "Copied to the clipboard! \n"
+
+  openssl enc -aes-256-cbc -salt -in $FILE -out file.txt.enc
+
+elif [[ $answer == "view" ]]; then
+
+  openssl enc -aes-256-cbc -d -in file.txt.enc -out yeet.txt
+  cat yeet.txt
+fi
